c6880a8afd86146c9eaf5b32951a9db1fa709cdc
Marcus Fernandes <marcus@mlabs.city>
2024-06-05 10:00:01 -0300

    Fix test formatting

--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,20 @@ module Test.Main where
 import Prelude
 
 import Aeson (encodeAeson)
-import Cardano.Serialization.Lib (PlutusList, bigInt_fromStr, bigNum_fromStr, constrPlutusData_new, cslToAesonViaBytes, hashPlutusData, packListContainer, plutusData_newConstrPlutusData, plutusData_newInteger, plutusList_new, transactionWitnessSet_new, transactionWitnessSet_setPlutusData)
+import Cardano.Serialization.Lib
+  ( PlutusList
+  , bigInt_fromStr
+  , bigNum_fromStr
+  , constrPlutusData_new
+  , cslToAesonViaBytes
+  , hashPlutusData
+  , packListContainer
+  , plutusData_newConstrPlutusData
+  , plutusData_newInteger
+  , plutusList_new
+  , transactionWitnessSet_new
+  , transactionWitnessSet_setPlutusData
+  )
 import Control.Monad.Error.Class (class MonadThrow)
 import Data.Maybe (fromJust)
 import Data.Nullable (Nullable, toMaybe)
@@ -23,15 +36,27 @@ nonNull :: forall a. Nullable a -> a
 nonNull x = unsafePartial $ fromJust $ toMaybe x
 
 -- Tests adapted from https://github.com/Emurgo/cardano-serialization-lib/blob/evgenii/conway_certs/rust/src/tests/plutus.rs
-legacyOutputRoundtrip :: forall a. Bind a => MonadEffect a => MonadThrow Error a => a Unit
+legacyOutputRoundtrip
+  :: forall a
+   . Bind a
+  => MonadEffect a
+  => MonadThrow Error a
+  => a Unit
 legacyOutputRoundtrip = do
   plutusList <- liftEffect plutusList_new
   let
-    constr0 = plutusData_newConstrPlutusData $ constrPlutusData_new (nonNull $ bigNum_fromStr "0") plutusList
+    constr0 = plutusData_newConstrPlutusData $
+      constrPlutusData_new (nonNull $ bigNum_fromStr "0") plutusList
     constr0Hash = encodeAeson $ hashPlutusData constr0
-  (show constr0Hash) `shouldEqual` "\"923918e403bf43c34b4ef6b48eb2ee04babed17320d8d1b9ff9ad086e86f44ec\""
+  (show constr0Hash) `shouldEqual`
+    "\"923918e403bf43c34b4ef6b48eb2ee04babed17320d8d1b9ff9ad086e86f44ec\""
 
-plutusListSerializationCliCompatibility :: forall a. Bind a => MonadEffect a => MonadThrow Error a => a Unit
+plutusListSerializationCliCompatibility
+  :: forall a
+   . Bind a
+  => MonadEffect a
+  => MonadThrow Error a
+  => a Unit
 plutusListSerializationCliCompatibility = do
   let
     plutusData = plutusData_newInteger (nonNull $ bigInt_fromStr "1")
@@ -50,5 +75,6 @@ spec :: Spec Unit
 spec = do
   describe "Plutus" do
     it "Legacy output roundtrip" legacyOutputRoundtrip
-    it "Plutus List Serialization CLI Compatibility" plutusListSerializationCliCompatibility
+    it "Plutus List Serialization CLI Compatibility"
+      plutusListSerializationCliCompatibility
 
